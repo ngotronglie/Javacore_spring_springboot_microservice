b27a7faf9f1f3f16edddd5a99693c57062747651
Ngô Trọng Liêm <ngotrongliem2004@gmail.com>
2025-04-12 20:16:51 +0700

    datatype and more

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        float marks = 10.0f;
+        boolean is_active = true;
+        Character cha = 'a';
         int a = 10;
         int b = 20;
         int c = a + b;
+        double d = 5.6;
+        byte by = 127;
+        char ch = 'a';
         System.out.println(c);
         System.out.println("Hello, World!");
     }
+}
+
+
+class Hello {
+    public int a;
+    public int b;
+    protected String username;
+
 }
